326c00a4d85c51f21ac2552a3d5f6901548c6961
jabauer <jabauer.1998@gmail.com>
2025-08-18 17:52:44 -0700

    Added some marcheling Code to convert to C in order to utilize llvm

--- src/haskell/CalcLangAst.hs
+++ src/haskell/CalcLangAst.hs
@@ -65,5 +65,97 @@ data AstNode = EqualOperation Pos AstNode AstNode
              | ErrorNode String
              deriving (Eq, Show)
 
+--Now we need to make the AstNodes Visible from C. This will be accomplished through the Storable class
+instance Storable SourcePos where
+  sizeOf _ = (sizeOf (undefined :: CInt)) * 2
+  alignment _ = ()
+instance Storable AstNode where
+    sizeOf _ = sizeOf (undefined :: CInt) + sizeOf (undefined :: Ptr Pos) + sizeOf (undefined :: Ptr AstNode) + sizeOf (undefined :: Ptr AstNode) -- Example size, adjust for actual fields
+    alignment _ = alignment (undefined :: CInt)
+
+    peek ptr = do
+        tag <- peekByteOff ptr 0 :: IO CInt
+        case tag of
+            0 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (EqualOperation pos leftPtr rightPtr)
+            1 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (LessThenOrEqualsOperation  pos leftPtr rightPtr)
+            2 <- do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (GreaterThenOrEqualsOperation  pos leftPtr rightPtr)
+            3->  do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (GreaterThenOperation  pos leftPtr rightPtr)
+            4 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (LessThenOperation  pos leftPtr rightPtr)
+            5 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (AdditionOperation  pos leftPtr rightPtr)
+            6 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (SubtractionOperation  pos leftPtr rightPtr)
+            7 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (MultiplicationOperation  pos leftPtr rightPtr)
+            8 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (DotProductOperation  pos leftPtr rightPtr)
+            9 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (DivisionOperation  pos leftPtr rightPtr)
+            10 -> do
+                 pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                 leftPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO (Ptr AstNode)
+                 rightPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos)) + (sizeOf (undefined :: Ptr AstNode))) :: IO (Ptr AstNode)
+                 return (PowerOperation  pos leftPtr rightPtr)
+            11 -> do
+                  pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                  strPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO CString
+                  return (IntNumberAst pos (peekCString strPtr))
+            12 -> do
+                  pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                  strPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO CString
+                  return (RealNumberAst pos (peekCString strPtr))
+            13 -> do
+                  pos <- peekByteOff ptr (sizeOf (undefined :: CInt)) :: IO (Ptr Pos)
+                  strPtr <- peekByteOff ptr ((sizeOf (undefined :: CInt)) + (sizeOf (undefined :: Ptr Pos))) :: IO CString
+                  return (BooleanAst pos (peekCString strPtr))
+            2 -> Var <$> peekByteOff ptr (sizeOf (undefined :: CInt))
+            _ -> error "Unknown AST node tag"
+
+    poke ptr (Lit i) = do
+        pokeByteOff ptr 0 (0 :: CInt) -- Tag for Lit
+        pokeByteOff ptr (sizeOf (undefined :: CInt)) i
+    poke ptr (Add l r) = do
+        pokeByteOff ptr 0 (1 :: CInt) -- Tag for Add
+        pokeByteOff ptr (sizeOf (undefined :: CInt)) l
+        pokeByteOff ptr (sizeOf (undefined :: CInt) + sizeOf (undefined :: Ptr Expr)) r
+    poke ptr (Var s) = do
+        pokeByteOff ptr 0 (2 :: CInt) -- Tag for Var
+        pokeByteOff ptr (sizeOf (undefined :: CInt)) s
+
 
 
