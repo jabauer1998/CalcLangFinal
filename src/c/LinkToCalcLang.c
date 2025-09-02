81d92e44acc2ee2ac499638ea368f36c8774227d
jabauer <jabauer.1998@gmail.com>
2025-09-02 15:32:11 -0700

    Added code to perform a dot product on tuples and to exponent integers and reals

--- src/c/LinkToCalcLang.c
+++ src/c/LinkToCalcLang.c
@@ -1,6 +1,7 @@
 #include "LinkToCalcLang.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 void freeCalcLangValue(CalcLangValue* val);
 
@@ -967,4 +968,271 @@ CalcLangValue* divCalcLangValues(CalcLangValue* left, int freeLeft, CalcLangValu
     freeCalcLangValue(right);
 
   return result;
+}
+
+CalcLangValue* powCalcLangValues(CalcLangValue* left, int freeLeft, CalcLangValue* right, int freeRight){
+  CalcLangValue* result = malloc(sizeof(CalcLangValue));
+  if(left->valType == IS_INT && right->valType == IS_INT){
+    result->valType = IS_INT;
+    result->valData.integer = (int)pow((double)(left->valData.integer),(double)(right->valData.integer));
+  } else if(left->valType == IS_REAL && right->valType == IS_REAL){
+    result->valType = IS_REAL;
+    result->valData.real = pow(left->valData.real, right->valData.real);
+  } else if(left->valType == IS_INT && right->valType == IS_REAL){
+    result->valType = IS_REAL;
+    result->valData.real = pow(((double)(left->valData.integer)), right->valData.real);
+  } else if(left->valType == IS_REAL && right->valType == IS_INT){
+    result->valType = IS_REAL;
+    result->valData.real = pow(left->valData.real, ((double)(right->valData.integer)));
+  } else if(left->valType == IS_DOLLAR && right->valType == IS_INT){
+    result->valType = IS_DOLLAR;
+    result->valData.dollar = pow(left->valData.dollar, ((double)right->valData.integer));
+  } else if(left->valType == IS_INT && right->valType == IS_DOLLAR){
+    result->valType = IS_DOLLAR;
+    result->valData.dollar = pow((double)(left->valData.integer), right->valData.dollar);
+  } else if(left->valType == IS_REAL && right->valType == IS_DOLLAR){
+    result->valType = IS_DOLLAR;
+    result->valData.dollar = pow(left->valData.real, right->valData.dollar);
+  } else if(left->valType == IS_DOLLAR && right->valType == IS_REAL){
+    result->valType = IS_DOLLAR;
+    result->valData.dollar = pow(left->valData.dollar, right->valData.real);
+  } else if(left->valType == IS_PERCENT && right->valType == IS_INT){
+    result->valType = IS_PERCENT;
+    result->valData.percent = pow(left->valData.percent, (double)(right->valData.integer));
+  } else if(left->valType == IS_INT && right->valType == IS_PERCENT){
+    result->valType = IS_PERCENT;
+    result->valData.percent = pow((double)(left->valData.integer), right->valData.percent);
+  } else if(left->valType == IS_REAL && right->valType == IS_PERCENT){
+    result->valType = IS_PERCENT;
+    result->valData.percent = pow(left->valData.real,  right->valData.percent);
+  } else if(left->valType == IS_PERCENT && right->valType == IS_REAL){
+    result->valType = IS_PERCENT;
+    result->valData.percent = pow(left->valData.percent, right->valData.real);
+  } else if(left->valType == IS_PERCENT && right->valType == IS_PERCENT){
+    result->valType = IS_PERCENT;
+    result->valData.percent = pow(left->valData.percent,  right->valData.percent); 
+  } else if(left->valType == IS_TUPLE && right->valType == IS_TUPLE){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* rightTup = right->valData.tuple;
+    if(rightTup->size == leftTup->size){
+      TupleValue* resultTup = malloc(sizeof(TupleValue));
+      resultTup->size = leftTup->size;
+      resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+      for(int i = 0; i < leftTup->size; i++){
+	resultTup->list[i] = multCalcLangValues(leftTup->list[i], TRUE, rightTup->list[i], FALSE);  
+      }
+      result->valType = IS_TUPLE;
+      result->valData.tuple = resultTup;
+    } else {
+      perror("Error Tuple sizes in expression do not match!!!");
+      return NULL;
+    }
+  } else if(left->valType == IS_TUPLE && right->valType == IS_INT){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = leftTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < leftTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(leftTup->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_INT && right->valType == IS_TUPLE){
+    TupleValue* rightTup = right->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = rightTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < rightTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(left, FALSE, rightTup->list[i], TRUE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_TUPLE && right->valType == IS_REAL){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = leftTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < leftTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(leftTup->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_REAL && right->valType == IS_TUPLE){
+    TupleValue* rightTup = right->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = rightTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < rightTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(left, FALSE, rightTup->list[i], TRUE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_TUPLE && right->valType == IS_DOLLAR){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = leftTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < leftTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(leftTup->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_DOLLAR && right->valType == IS_TUPLE){
+    TupleValue* rightTup = right->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = rightTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < rightTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(left, FALSE,  rightTup->list[i], TRUE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_TUPLE && right->valType == IS_PERCENT){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = leftTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < leftTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(leftTup->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_PERCENT && right->valType == IS_TUPLE){
+    TupleValue* rightTup = right->valData.tuple;
+    TupleValue* resultTup = malloc(sizeof(TupleValue));
+    resultTup->size = rightTup->size;
+    resultTup->list = malloc(sizeof(CalcLangValue*) * resultTup->size);
+    for(int i = 0; i < rightTup->size; i++){
+      resultTup->list[i] = powCalcLangValues(left, FALSE, rightTup->list[i], TRUE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.tuple = resultTup;
+  } else if(left->valType == IS_SET && right->valType == IS_SET){
+    SetValue* leftSet = left->valData.set;
+    SetValue* rightSet = right->valData.set;
+    if(rightSet->size == leftSet->size){
+      SetValue* resultSet = malloc(sizeof(SetValue));
+      resultSet->size = leftSet->size;
+      resultSet->list = malloc(sizeof(SetValue*) * resultSet->size);
+      for(int i = 0; i < leftSet->size; i++){
+	resultSet->list[i] = powCalcLangValues(leftSet->list[i], TRUE, rightSet->list[i], TRUE);  
+      }
+      result->valType = IS_SET;
+      result->valData.set = resultSet;
+    } else {
+      perror("Error Tuple sizes in expression do not match!!!");
+      return NULL;
+    }
+  } else if(left->valType == IS_SET && right->valType == IS_INT){
+    SetValue* leftSet = left->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = leftSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < leftSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(leftSet->list[i], TRUE, right, FALSE);
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_INT && right->valType == IS_SET){
+    SetValue* rightSet = right->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = rightSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < rightSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(left, FALSE, rightSet->list[i], TRUE);  
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_SET && right->valType == IS_REAL){
+    SetValue* leftSet = left->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = leftSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < leftSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(leftSet->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_TUPLE;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_REAL && right->valType == IS_SET){
+    SetValue* rightSet = right->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = rightSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < rightSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(left, FALSE, rightSet->list[i], TRUE);  
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_SET && right->valType == IS_DOLLAR){
+    SetValue* leftSet = left->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = leftSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < leftSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(leftSet->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_DOLLAR && right->valType == IS_SET){
+    SetValue* rightSet = right->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = rightSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < rightSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(left, FALSE, rightSet->list[i], TRUE);
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_SET && right->valType == IS_PERCENT){
+    SetValue* leftSet = left->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = leftSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < leftSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(leftSet->list[i], TRUE, right, FALSE);  
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else if(left->valType == IS_PERCENT && right->valType == IS_SET){
+    SetValue* rightSet = right->valData.set;
+    SetValue* resultSet = malloc(sizeof(SetValue));
+    resultSet->size = rightSet->size;
+    resultSet->list = malloc(sizeof(CalcLangValue*) * resultSet->size);
+    for(int i = 0; i < rightSet->size; i++){
+      resultSet->list[i] = powCalcLangValues(left, FALSE, rightSet->list[i], TRUE);  
+    }
+    result->valType = IS_SET;
+    result->valData.set = resultSet;
+  } else {
+    perror("Invalid types found for CalcLangAddition Operation");
+  }
+
+  if(freeLeft)
+    freeCalcLangValue(left);
+  else if(freeRight)
+    freeCalcLangValue(right);
+
+  return result;
+}
+
+CalcLangValue* dotProductVals(CalcLangValue* left, int freeLeft, CalcLangValue* right, int freeRight){
+  if(left->valType == IS_TUPLE && right->valType == IS_TUPLE){
+    TupleValue* leftTup = left->valData.tuple;
+    TupleValue* rightTup = right->valData.tuple;
+    if(leftTup->size == rightTup->size){
+      CalcLangValue* total = newIntValue(0);
+      for(int i = 0; i < leftTup->size; i++){
+	CalcLangValue* leftTupVal = leftTup->list[i];
+	CalcLangValue* rightTupVal = rightTup->list[i];
+	CalcLangValue* multResult = multCalcLangValues(leftTupVal, FALSE, rightTupVal, FALSE);
+	total = addCalcLangValues(total, TRUE, multResult, TRUE);
+      }
+
+      return total;
+    } else {
+      perror("Mismatched tup for size");
+    }
+  } else {
+    perror("Invalid type for dot product expected Tuple and Tuple");
+  }
 }
